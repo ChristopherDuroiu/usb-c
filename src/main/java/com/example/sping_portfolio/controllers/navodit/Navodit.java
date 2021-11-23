fb0b5a91d94ea4bb507696fb4686b99504b68148
Navodit1603 <navoditmaheshwari@gmail.com>
2021-11-22 17:04:45 -0800

    navodit image change

--- src/main/java/com/example/sping_portfolio/controllers/navodit/Navodit.java
+++ src/main/java/com/example/sping_portfolio/controllers/navodit/Navodit.java
@@ -1,12 +1,32 @@
 package com.example.sping_portfolio.controllers.navodit;
 
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Navodit {
     @GetMapping("/navodit")
-    public String navodit() {
+    public String navodit(){
+
         return "navodit";
     }
 }
