186e1e6d2afe15b60f3d419656ce30b700c5f34d
gracele246 <73254122+gracele246@users.noreply.github.com>
2021-11-29 10:03:43 -0800

    aboutme page

--- src/main/java/com/example/sping_portfolio/controllers/grace/Grace.java
+++ src/main/java/com/example/sping_portfolio/controllers/grace/Grace.java
@@ -1,10 +1,38 @@
 package com.example.sping_portfolio.controllers.grace;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Controller
 public class Grace {
     @GetMapping("/grace")
-    public String grace() {return "grace";}
+    public String grace(Model model) throws IOException, InterruptedException, ParseException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia"))
+                .header("x-rapidapi-host", "trivia-by-api-ninjas.p.rapidapi.com")
+                .header("x-rapidapi-key", "845637a39cmshcd1cb3388e6f02ap15ffd5jsne437b834d421")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        Object object = new JSONParser().parse(response.body());
+        JSONArray trivia = (JSONArray) object;
+
+        //model.addAttribute("trivia", trivia);
+
+        {return "grace";}
+    }
+
 }
