b141c3b27587420644ed54c2fb885d4e790b5216
calebkimsd <89210382+calebkimsd@users.noreply.github.com>
2021-12-06 22:23:48 -0800

    create intial frq page

---
+++ src/main/java/com/example/sping_portfolio/controllers/caleb/LightSequenceCaleb.java
@@ -0,0 +1,12 @@
+package com.example.sping_portfolio.controllers.caleb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class LightSequenceCaleb {
+
+
+
+}
