5b953014982d8d86e2db0760099692c7e5e308cb
gracele246 <73254122+gracele246@users.noreply.github.com>
2021-12-09 08:58:28 -0800

    api and thymleaf

--- src/main/java/com/example/sping_portfolio/controllers/grace/Grace.java
+++ src/main/java/com/example/sping_portfolio/controllers/grace/Grace.java
@@ -6,6 +6,7 @@ import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.net.URI;
@@ -13,15 +14,16 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
 @Controller
 public class Grace {
     @GetMapping("/grace")
     public String grace(Model model) throws IOException, InterruptedException, ParseException {
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia"))
-                .header("x-rapidapi-host", "trivia-by-api-ninjas.p.rapidapi.com")
-                .header("x-rapidapi-key", "845637a39cmshcd1cb3388e6f02ap15ffd5jsne437b834d421")
+                .uri(URI.create("https://numbersapi.p.rapidapi.com/10/trivia?fragment=true&notfound=floor&json=true"))
+                .header("x-rapidapi-host", "numbersapi.p.rapidapi.com")
+                .header("x-rapidapi-key", "215a0875bcmsh7b230e4f9ab5a5dp1b2cf9jsn0b45c55a9a92")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
@@ -30,9 +32,12 @@ public class Grace {
         Object object = new JSONParser().parse(response.body());
         JSONArray trivia = (JSONArray) object;
 
-        //model.addAttribute("trivia", trivia);
+        model.addAttribute("trivia", trivia);
 
+        //model.addAttribute("trivia", trivia);
         {return "grace";}
     }
 
 }
+
+
