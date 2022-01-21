0ea2640635a9b32e02c1cb2fca43148d988b9501
Navodit1603 <navoditmaheshwari@gmail.com>
2022-01-20 22:02:43 -0800

    boolean

---
+++ src/main/java/com/example/sping_portfolio/controllers/navodit/Ending.java
@@ -0,0 +1,13 @@
+package com.example.sping_portfolio.controllers.navodit;
+
+public class Ending {
+
+    String[] m_words;
+
+    public Ending(String[] words){
+
+        m_words = words;
+    }
+
+
+}
